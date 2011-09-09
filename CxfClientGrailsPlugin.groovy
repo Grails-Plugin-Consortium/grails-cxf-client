7625a67aba65d1acbd5248c0e3b7e6739e9d239a
Christian Oestreich <acetrike@yahoo.com>
2011-09-09 11:11:31 -0500

    removing onChange for now, need to understand this better

--- CxfClientGrailsPlugin.groovy
+++ CxfClientGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class CxfClientGrailsPlugin {
     // the plugin version
     def version = "0.2"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "1.3.6 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
