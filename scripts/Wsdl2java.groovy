a4e04a37c1d346d999c7a599a702ffeb1f03ff4e
Christian Oestreich <acetrike@yahoo.com>
2011-09-09 16:32:49 -0500

    Adding initial support for wsd2java target

---
+++ scripts/Wsdl2java.groovy
@@ -0,0 +1,60 @@
+includeTargets << grailsScript("_GrailsSettings")
+includeTargets << grailsScript('_GrailsPackage')
+includeTargets << grailsScript("_GrailsArgParsing")
+includeTargets << grailsScript("Init")
+
+target(main: '''generate java class stubs out wsdl files.
+This target needs to be run only upon changes in the upstream API, since it's artefacts are kept under version control in src/java
+''') {
+
+    depends(compile, createConfig, parseArguments)
+
+    if(!config.cxf.home){
+        echo "ERROR: You must set the config property for cxf.home to the root dir of your apache cxf install"
+        echo "eg: cxf { home = \"c:/apache-cxf-2.4.2\" } }"
+        echo "Please set this and try again"
+        return
+    }
+
+    echo "starting wsdl2java"
+    def wsdls = []
+    def cxflib = "${config.cxf.home}/lib"
+    config.cxf.client.each {
+        wsdls << it?.value?.wsdl
+    }
+
+    if(!new File(cxflib).exists()){
+        echo "ERROR: You must set the config property for cxf.home to the root dir of your apache cxf install"
+        echo "eg: cxf { home = \"c:/apache-cxf-2.4.2\" } }"
+        echo "Your dir ${cxflib} does not appear to exist"
+        echo "Please correct this and try again"
+        return
+    }
+
+    path(id: "classpath") {
+        fileset(dir: cxflib)
+    }
+
+    wsdls.each { wsdl ->
+        echo "generating java stubs from $wsdl"
+
+        if(wsdl) {
+            java(fork: true, classpathref: "classpath", classname: "org.apache.cxf.tools.wsdlto.WSDLToJava") {
+                arg(value: "-verbose")
+                arg(value: "-client")
+                //arg(value: "-b")
+                //arg(value: "grails-app/conf/bindings.xml")
+                arg(value: "-d")
+                arg(value: "src/java")
+                //arg(value: "-catalog")
+                //arg(value: "src/java/META-INF/jax-ws-catalog.xml")
+                arg(value: wsdl)
+            }
+        }
+    }
+
+    echo "completed wsdl2java"
+}
+
+setDefaultTarget(main)
+
