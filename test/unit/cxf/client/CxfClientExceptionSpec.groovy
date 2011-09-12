900dbde722c3f73b99f38c71cd4f9330e9664657
Christian Oestreich <acetrike@yahoo.com>
2011-09-12 12:39:53 -0500

    Cleaning up code based on full codenarc ruleset

--- test/unit/cxf/client/CxfClientExceptionSpec.groovy
+++ test/unit/cxf/client/CxfClientExceptionSpec.groovy
@@ -1,20 +1,69 @@
 package cxf.client
 
-import spock.lang.Specification
 import com.grails.cxf.client.exception.CxfClientException
+import spock.lang.Specification
 
 class CxfClientExceptionSpec extends Specification {
 
-    def "normal exception behavior test"() {
+    def "normal exception behavior test with throwable and message"() {
+        given:
+        String message = "detailMessage"
+        Throwable throwable = new Throwable(message)
+
         when:
-        throwException()
+        throwException(throwable, message)
 
         then:
         CxfClientException e = thrown()
         e.message == "detailMessage"
     }
 
+    def "normal exception behavior test with throwable"() {
+        given:
+        Throwable throwable = new Throwable("some throwable")
+
+        when:
+        throwException(throwable)
+
+        then:
+        CxfClientException e = thrown()
+        e.message == "java.lang.Throwable: some throwable"
+    }
+
+    def "normal exception behavior test no message"() {
+        when:
+        throwException()
+
+        then:
+        CxfClientException e = thrown()
+        !e.message
+    }
+
+    def "normal exception behavior test"() {
+        given:
+        String message = "detailMessage"
+
+        when:
+        throwException(message)
+
+        then:
+        CxfClientException e = thrown()
+        e.message == message
+    }
+
     def throwException() throws CxfClientException {
-        throw new CxfClientException("detailMessage")
+        throw new CxfClientException()
+    }
+
+    def throwException(String message) throws CxfClientException {
+        throw new CxfClientException(message)
+    }
+
+    def throwException(Throwable throwable) throws CxfClientException {
+        throw new CxfClientException(throwable)
+    }
+
+    def throwException(Throwable throwable, String message) throws CxfClientException {
+        throw new CxfClientException(message, throwable)
     }
 }
