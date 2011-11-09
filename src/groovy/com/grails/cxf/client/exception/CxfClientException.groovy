81dd9d12528170cd9b62011feaf2591e143e612c
Christian Oestreich <acetrike@yahoo.com>
2011-11-09 16:35:09 -0600

    I guess codenarc conflicts on serialVersionUID rule and FieldName rule

--- src/groovy/com/grails/cxf/client/exception/CxfClientException.groovy
+++ src/groovy/com/grails/cxf/client/exception/CxfClientException.groovy
@@ -7,7 +7,7 @@ class CxfClientException extends Throwable implements Serializable {
     /**
      * Serialization Version ID compatibility - indication of what version may be restored.
      */
-    private static final long SerialVersionUID = 13312399123L
+    private static final long serialVersionUID = 13312399123L
 
     /**
      * Creates a new instance of UpdateException
