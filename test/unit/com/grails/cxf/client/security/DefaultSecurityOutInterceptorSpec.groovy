badd99b2915b619a56340c864a5b0b789556a8d4
Christian Oestreich <acetrike@yahoo.com>
2012-03-01 16:56:36 -0600

    moving tests to more appropriate directories

--- test/unit/com/grails/cxf/client/security/DefaultSecurityOutInterceptorSpec.groovy
+++ test/unit/com/grails/cxf/client/security/DefaultSecurityOutInterceptorSpec.groovy
@@ -3,12 +3,14 @@ package com.grails.cxf.client.security
 import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor
 import org.apache.ws.security.handler.WSHandlerConstants
 import spock.lang.Specification
+import com.grails.cxf.client.exception.CxfClientException
+import javax.security.auth.callback.CallbackHandler
 
 /**
  */
 class DefaultSecurityOutInterceptorSpec extends Specification {
 
-    def "default security out interceptor injection test"() {
+    def "construct default security out interceptor"() {
         given:
         DefaultSecurityOutInterceptor defaultSecurityOutInterceptor = new DefaultSecurityOutInterceptor(username: "bob", password: "pass")
 
@@ -19,5 +21,30 @@ class DefaultSecurityOutInterceptorSpec extends Specification {
         wss4j instanceof WSS4JOutInterceptor
         wss4j.getOption(WSHandlerConstants.USER) == "bob"
         wss4j.getOption(WSHandlerConstants.PASSWORD_TYPE) == org.apache.ws.security.WSConstants.PW_TEXT
+        wss4j.getOption(WSHandlerConstants.PW_CALLBACK_REF) instanceof CallbackHandler
+    }
+
+    def "set default security out interceptor with empty password"() {
+        given:
+        DefaultSecurityOutInterceptor defaultSecurityOutInterceptor = new DefaultSecurityOutInterceptor(username: "bob", password: "")
+
+        when:
+        def wss4j = defaultSecurityOutInterceptor.create()
+
+        then:
+        !wss4j
+        thrown(CxfClientException)
+    }
+
+    def "set default security out interceptor with null password"() {
+        given:
+        DefaultSecurityOutInterceptor defaultSecurityOutInterceptor = new DefaultSecurityOutInterceptor(username: "bob", password: null)
+
+        when:
+        def wss4j = defaultSecurityOutInterceptor.create()
+
+        then:
+        !wss4j
+        thrown(CxfClientException)
     }
 }
