6b6ee4f4bf249fdb28243e777aaab90360a155b0
Mathi Fonseca <mathifonseca@gmail.com>
2016-08-11 16:54:34 -0300

    v2.2.0

--- CxfClientGrailsPlugin.groovy
+++ CxfClientGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class CxfClientGrailsPlugin {
     private final Long DEFAULT_CONNECTION_TIMEOUT = 30000
     private final Long DEFAULT_RECEIVE_TIMEOUT = 60000
 
-    def version = "2.1.2"
+    def version = "2.2.0"
     def grailsVersion = "2.2 > *"
     def pluginExcludes = [
             'grails-app/conf/codenarc.groovy',
